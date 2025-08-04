f5f4fe06599fd0eb064549265fd14bf09f467c1b
akash-HP <akashhp509@gmail.com>
2025-08-04 12:32:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold text-blue-600">Hello Tailwind!</h1>
+  );
+}
+
+export default App;
